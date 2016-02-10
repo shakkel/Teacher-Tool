fcd6ebf3f45a981d196c99c163f7e9f154f18aaa
Speidy674 <marvin33@live.de>
2016-02-10 12:32:15 +0100

    Website
    Website

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Teacher Tool</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+ <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Teacher Tool</a>
+    </div>
+    <ul class="nav navbar-nav">
+	  
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Klassen<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li id="1drop1-l1"><a id="1drop1-a" href="#">Dropdown 1-1</a></li>
+          <li id="1drop2-li"><a id="1drop2-a" href="#">Dropdown 1-2</a></li>
+          <li id="1drop3-li"><a id="1drop3-a" href="#">Dropdown 1-3</a></li>
+            <li id="1drop4-l1"><a id="1drop4-a" href="#">Dropdown 1-4</a></li>
+          <li id="1drop5-li"><a id="1drop5-a" href="#">Dropdown 1-5</a></li>
+          <li id="1drop6-li"><a id="1drop6-a" href="#">Dropdown 1-6</a></li>
+            <li id="1drop7-l1"><a id="1drop7-a" href="#">Dropdown 1-7</a></li>
+          <li id="1drop8-li"><a id="1drop8-a" href="#">Dropdown 1-8</a></li>
+          <li id="1drop9-li"><a id="1drop9-a" href="#">Dropdown 1-9</a></li>
+        </ul>
+      </li>
+	
+      <li id="btn1-li"><a id="btn1-a" href="#">Formulare anpassen</a></li>
+      <li id="btn2-li"><a id="btn2-a" href="#">Schüler</a></li>
+      <li id="btn3-li"><a id="btn3-a" href="#">Anwesenheit</a></li>
+      <li id="btn4-li"><a id="btn4-a" href="#">Formulare</a></li>
+      <li id="btn5-li"><a id="btn5-a" href="#">Hausaufgaben</a></li>
+        <li id="btn6-li"><a id="btn6-a" href="#">Sonstiges</a></li>
+	  
+	  
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-option-horizontal"></span> Optionen</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+    </ul>
+  </div>
+</nav>
+
+
+<div class="container">
+  <h1>Willkommen</h1>
+  <p>hier könnte ihre Werbung Stehen</p>
+</div>
+
+</body>
+</html>
